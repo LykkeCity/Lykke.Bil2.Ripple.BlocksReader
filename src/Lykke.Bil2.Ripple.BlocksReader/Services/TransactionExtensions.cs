5bacff9ebdc8b45c0aad12008f5951e5f1450b90
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2019-05-30 12:36:09 +0300

    tests fixed

--- src/Lykke.Bil2.Ripple.BlocksReader/Services/TransactionExtensions.cs
+++ src/Lykke.Bil2.Ripple.BlocksReader/Services/TransactionExtensions.cs
@@ -8,8 +8,9 @@ namespace Lykke.Bil2.Ripple.BlocksReader.Services
     public static class TransactionExtensions
     {
         // Well known tx hash duplications
-        private static readonly IReadOnlyDictionary<(BlockId blockId, string transactionHash), long>  Violations = new Dictionary<(BlockId blockId, string transactHash), long>
+        private static readonly IReadOnlyDictionary<(BlockId blockId, string transactionHash), long?>  Violations = new Dictionary<(BlockId blockId, string transactHash), long?>
         {
+            {("3BACFC2C30CF4B124FCFF2B2539C8B400B11249A3C3D8AC80F7E558F0E7B062F", "C6A40F56127436DCD830B1B35FF939FD05B5747D30D6542572B7A835239817AF"), null},
             {("644732C14FA656B17770EC50653B5B1D5892D237ED3254A4ABB574AA64F0D82A", "C6A40F56127436DCD830B1B35FF939FD05B5747D30D6542572B7A835239817AF"), 0},
         };
 
@@ -24,7 +25,7 @@ namespace Lykke.Bil2.Ripple.BlocksReader.Services
 
             if (Violations.TryGetValue((blockId, txHash), out var suffix))
             {
-                return $"{txHash}_{suffix}";
+                return suffix.HasValue ? $"{txHash}_{suffix}" : txHash;
             }
 
             if (SuspiciousTransactionHashes.Contains(txHash))
